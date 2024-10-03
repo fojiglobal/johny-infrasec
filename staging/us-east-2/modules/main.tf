7c7461998db2c14298538af0eece7f15a5551399
Jonathan Fotso <jonasfost@gmail.com>
2024-10-02 21:55:37 -0400

    updated private route table to create a route to nat gateway instead of internat gateway

--- staging/us-east-2/modules/main.tf
+++ staging/us-east-2/modules/main.tf
@@ -226,7 +226,7 @@ resource "aws_route_table" "private_rt" {
 
   route {
     cidr_block = var.all_ipv4_cidr
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_nat_gateway.ngw.id
   }
 
   tags = {
