c649e6eb2034105d8989a1792b8b56636105e8c6
Jonathan Fotso <jonasfost@gmail.com>
2024-12-21 15:21:42 -0500

    Updatef waf.tf file

--- staging/us-east-2/waf.tf
+++ staging/us-east-2/waf.tf
@@ -50,7 +50,7 @@ resource "aws_wafv2_web_acl" "staging_web_acl" {
     }
   }
 
-  # # Anonymous IP list AWS Managed rule group
+  # Anonymous IP list AWS Managed rule group
   rule {
     name     = "AWSManagedRulesAnonymousIpList"
     priority = 3
@@ -70,7 +70,7 @@ resource "aws_wafv2_web_acl" "staging_web_acl" {
     }
   }
 
-  # # Core rule set AWS Managed rule group
+  # Core rule set AWS Managed rule group
   rule {
     name     = "AWSManagedRulesCommonRuleSet"
     priority = 4
@@ -90,7 +90,7 @@ resource "aws_wafv2_web_acl" "staging_web_acl" {
     }
   }
 
-  # # Known bad inputs AWS Managed rule group
+  # Known bad inputs AWS Managed rule group
   rule {
     name     = "AWSManagedRulesKnownBadInputsRuleSet"
     priority = 5
@@ -110,7 +110,7 @@ resource "aws_wafv2_web_acl" "staging_web_acl" {
     }
   }
 
-  # # Linux operating system AWS Managed rule group
+  # Linux operating system AWS Managed rule group
   rule {
     name     = "AWSManagedRulesLinuxRuleSet"
     priority = 6
@@ -130,7 +130,7 @@ resource "aws_wafv2_web_acl" "staging_web_acl" {
     }
   }
 
-  # # PHP application AWS Managed rule group
+  # PHP application AWS Managed rule group
   rule {
     name     = "AWSManagedRulesPHPRuleSet"
     priority = 7
@@ -150,85 +150,85 @@ resource "aws_wafv2_web_acl" "staging_web_acl" {
     }
   }
 
-  # # POSIX operating system AWS Managed rule group
-  # rule {
-  #   name     = "AWSManagedRulesPOSIXRuleSet"
-  #   priority = 8
-  #   override_action {
-  #     count {}
-  #   }
-  #   statement {
-  #     managed_rule_group_statement {
-  #       name        = "AWSManagedRulesPOSIXRuleSet"
-  #       vendor_name = "AWS"
-  #     }
-  #   }
-  #   visibility_config {
-  #     sampled_requests_enabled   = true
-  #     cloudwatch_metrics_enabled = true
-  #     metric_name                = "AWSManagedRulesPOSIXRuleSet"
-  #   }
-  # }
+  # POSIX operating system AWS Managed rule group
+  rule {
+    name     = "AWSManagedRulesPOSIXRuleSet"
+    priority = 8
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesUnixRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesPOSIXRuleSet"
+    }
+  }
 
-  # # SQL database AWS Managed rule group
-  # rule {
-  #   name     = "AWSManagedRulesSQLiRuleSet"
-  #   priority = 9
-  #   override_action {
-  #     count {}
-  #   }
-  #   statement {
-  #     managed_rule_group_statement {
-  #       name        = "AWSManagedRulesSQLiRuleSet"
-  #       vendor_name = "AWS"
-  #     }
-  #   }
-  #   visibility_config {
-  #     sampled_requests_enabled   = true
-  #     cloudwatch_metrics_enabled = true
-  #     metric_name                = "AWSManagedRulesSQLiRuleSet"
-  #   }
-  # }
+  # SQL database AWS Managed rule group
+  rule {
+    name     = "AWSManagedRulesSQLiRuleSet"
+    priority = 9
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesSQLiRuleSet"
+    }
+  }
 
-  # # Windows operating system AWS Managed rule group
-  # rule {
-  #   name     = "AWSManagedRulesWindowsRuleSet"
-  #   priority = 10
-  #   override_action {
-  #     count {}
-  #   }
-  #   statement {
-  #     managed_rule_group_statement {
-  #       name        = "AWSManagedRulesWindowsRuleSet"
-  #       vendor_name = "AWS"
-  #     }
-  #   }
-  #   visibility_config {
-  #     sampled_requests_enabled   = true
-  #     cloudwatch_metrics_enabled = true
-  #     metric_name                = "AWSManagedRulesWindowsRuleSet"
-  #   }
-  # }
+  # Windows operating system AWS Managed rule group
+  rule {
+    name     = "AWSManagedRulesWindowsRuleSet"
+    priority = 10
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesWindowsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesWindowsRuleSet"
+    }
+  }
 
-  # # WordPress application AWS Managed rule group
-  # rule {
-  #   name     = "AWSManagedRulesWordPressRuleSet"
-  #   priority = 11
-  #   override_action {
-  #     count {}
-  #   }
-  #   statement {
-  #     managed_rule_group_statement {
-  #       name        = "AWSManagedRulesWordPressRuleSet"
-  #       vendor_name = "AWS"
-  #     }
-  #   }
-  #   visibility_config {
-  #     sampled_requests_enabled   = true
-  #     cloudwatch_metrics_enabled = true
-  #     metric_name                = "AWSManagedRulesWordPressRuleSet"
-  #   }
-  # }
+  # WordPress application AWS Managed rule group
+  rule {
+    name     = "AWSManagedRulesWordPressRuleSet"
+    priority = 11
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesWordPressRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      sampled_requests_enabled   = true
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesWordPressRuleSet"
+    }
+  }
 
   visibility_config {
     cloudwatch_metrics_enabled = true
